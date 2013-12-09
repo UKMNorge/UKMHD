89402f826659681236065efb13efd74efaf1f37d
Marius Mandal <mariusmandal@gmail.com>
2013-12-09 13:28:22 +0100

    Google analytics

--- index.php
+++ index.php
@@ -50,15 +50,17 @@ window.fbAsyncInit = function() {
 <?php require_once('hd-public/'.$mod.'.inc.php'); ?>
 
 <script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-27157707-9']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+ 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-46216680-6']);
+  _gaq.push(['_trackPageview']);
+ 
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+ 
 </script>
 </body>
 </html>
